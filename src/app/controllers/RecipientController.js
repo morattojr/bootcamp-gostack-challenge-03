90fb24619fed75d5af10fca98d6463c2fceb79e7
Geraldo <morattojr@gmail.com>
2020-04-17 11:45:44 -0300

    adding all files

---
+++ src/app/controllers/RecipientController.js
@@ -0,0 +1,68 @@
+import * as Yup from 'yup';
+import Recipient from '../models/Recipient';
+import User from '../models/User';
+
+class RecipientController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      address: Yup.string().required(),
+      number: Yup.number().required(),
+      complement: Yup.string().required(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      zipcode: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const recipientExists = await Recipient.findOne({
+      where: { name: req.body.name },
+    });
+
+    if (recipientExists) {
+      return res.status(400).json({ error: 'Recipient already exists' });
+    }
+
+    const user = await User.findByPk(req.userId);
+
+    // check if logged user is admin
+    if (!user.admin) {
+      return res
+        .status(401)
+        .json({ error: 'Only Admin role accounts can create Recipients.' });
+    }
+
+    const recipient = await Recipient.create(req.body);
+
+    return res.json(recipient);
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      address: Yup.string(),
+      number: Yup.number(),
+      complement: Yup.string(),
+      state: Yup.string(),
+      city: Yup.string(),
+      zipcode: Yup.number(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { id } = req.body;
+
+    const recipient = await Recipient.findOne({ id: { id } });
+
+    const result = await await recipient.update(req.body);
+
+    return res.json(result);
+  }
+}
+
+export default new RecipientController();
---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+
+import authConfig from '../../config/auth';
+
+export default async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token not provided.' });
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    // default callback
+    // jwt.verify(token, secret, (err, result) => {
+    // })
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+    req.userId = decoded.id;
+    // req.admin = decoded.admin;
+
+    return next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Token invalid.' });
+  }
+};
